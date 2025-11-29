67841bdaff8de061e1b22f378102ee44b83734b8
cxz <1054757289@qq.com>
2025-11-29 22:06:18 +0800

    refactor(server): 移除未使用的URLRecord模型同步代码

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 import app from "./app.js"
-import URLRecord from "./models/urlRecordModel"
-
 const port = 3000
-await URLRecord.sync({ force: true })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
